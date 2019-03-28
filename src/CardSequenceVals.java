69a0d1f2b4fd00307d441b32113611207322e898
Kyle Kingsberry <kkingsbe@gmail.com>
2019-03-28 14:19:11 -0400

    Started figuring out card sequences

--- src/CardSequenceVals.java
+++ src/CardSequenceVals.java
@@ -3,4 +3,5 @@ public class CardSequenceVals {
     public static String[] powersTen = {"1", "10", "100", "1000", "Longer"};
     public static String[] numerical = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Longer"};
     public static String[] powersTwo = {"2", "4", "8", "16", "32", "64", "128", "256", "Longer"};
+    public static String[] randomAssShit = {"1","2","3","5","8","13","20","40","100"};
 }
