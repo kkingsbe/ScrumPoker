49059cc67950dcc145d6f26a8e2fd6f234421349
Noah <noah@ontheplanet.net>
2019-03-29 20:01:51 -0400

    Basic Home page

--- src/home.java
+++ src/home.java
@@ -1,5 +1,4 @@
-package Servlets;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "createGame", urlPatterns = "/createGame")
-public class createGame extends HttpServlet {
+@WebServlet(name = "home", urlPatterns = "/home")
+public class home extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("homePage.jsp");
+        dispatcher.forward(request, response);
     }
 }
