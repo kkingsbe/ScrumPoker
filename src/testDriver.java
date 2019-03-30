6ea69c68739782cf31d7995353c3296c71e709a1
Noah <noah@ontheplanet.net>
2019-03-29 20:15:14 -0400

    Basic Home page

--- src/testDriver.java
+++ src/testDriver.java
@@ -4,14 +4,14 @@ public class testDriver {
     public static void main(String[] args)
     {
         GameContainer container = new GameContainer(1000);
-        String gameId = container.newGame("Test Game", "Test game to verify shit works", Game.CardSequence.FIBONACHI);
-        System.out.println("Game created with id: " + gameId);
+        //String gameId = container.newGame("Test Game", "Test game to verify shit works", Game.CardSequence.FIBONACHI);
+       // System.out.println("Game created with id: " + gameId);
 
         Player player1 = new Player("Kyle"); //creates players
         Player player2 = new Player("Johnny");
 
-        container.getGame(gameId).addPlayer(player1);
-        container.getGame(gameId).addPlayer(player2);
+       // container.getGame(gameId).addPlayer(player1);
+       // container.getGame(gameId).addPlayer(player2);
 
         player1.setDesiredTime("5"); //adds the desired times
         player2.setDesiredTime("5");
