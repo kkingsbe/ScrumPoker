025f08a29222c78f0fd72cbecb1b16aa8a6405ca
Noah <noah@ontheplanet.net>
2019-04-02 19:39:50 -0400

    Basic Home page

--- src/beginVote.java
+++ src/beginVote.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "beginVote")
+@WebServlet(name = "beginVote", urlPatterns = "/beginVote")
 public class beginVote extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
